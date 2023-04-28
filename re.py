7285c2745f312c403cbc9aade5818cb9ff19102f
BOMBFUOCK <94738020+BOMBFUOCK@users.noreply.github.com>
2023-04-28 20:08:50 +0800

    1

--- re.py
+++ re.py
@@ -31,16 +31,12 @@ with st.sidebar:
     ax.bar(x,y)
     st.pyplot(fig)
 
-# 获取本地H264文件的路径
 file_path = "/People-Counting-in-Real-Time-master/People-Counting-in-Real-Time-master/output1.h264"
 
-# 将H264文件转换为MP4格式
 converted_file_path = os.path.splitext(file_path)[0] + ".mp4"
 command = ["ffmpeg", "-y", "-i", file_path, "-c:v", "libx264", "-preset", "ultrafast", "-crf", "23", "-c:a", "aac", "-b:a", "128k", converted_file_path]
 subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# 检查转换后的文件是否存在
 if os.path.exists(converted_file_path):
-    # 在Streamlit应用程序中呈现视频
     video_bytes = open(converted_file_path, 'rb').read()
     st.video(video_bytes)
